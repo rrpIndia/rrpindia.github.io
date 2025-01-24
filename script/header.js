c4bc834c3c9d7eb3fa868f4831fd525f3b5f594b
UP61 Sartaj Ansari <ssaarrttaajj111@gmail.com>
2025-01-24 21:52:13 +0530

    added hEADER JS

---
+++ script/header.js
@@ -0,0 +1,18 @@
+document.addEventListener('DOMContentLoaded', () => {
+    fetch('html/header.html') // Fetch the header HTML file
+      .then(response => {
+        if (!response.ok) {
+          throw new Error('Failed to load header');
+        }
+        return response.text();
+      })
+      .then(data => {
+        // Insert the fetched HTML into the element with ID "footer"
+        document.getElementById('header').innerHTML = data;
+      })
+      .catch(error => {
+        console.error('Error loading footer:', error);
+        // Optional fallback message
+        document.getElementById('header').innerHTML = '<p>header could not be loaded.</p>';
+      });
+  });
